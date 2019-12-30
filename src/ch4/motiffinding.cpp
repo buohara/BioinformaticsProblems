3de45711be3072efe1a1368fe61ca1d04986400f
Benjamin O'Hara <buohara@gmail.com>
2019-12-29 23:24:00 -0800

    Get a first brute force implementation of motif finding working. At least one test run gets correct answer for small random case (6 sequences of length 10, random motif of length 6).

--- src/ch4/motiffinding.cpp
+++ src/ch4/motiffinding.cpp
@@ -1,6 +1,6 @@
 #include "problems.h"
 
-static void GenerateMotifSequences(uint32_t nSeq, uint32_t seqLen, uint32_t motifLen, vector<string>& seq, string& motif, vector<uint32_t> &offsets)
+static void GenerateMotifSequences(uint32_t nSeq, uint32_t seqLen, uint32_t motifLen, vector<string>& seqs, string& motif, vector<uint32_t> &offsets)
 {
     const char bases[4] = { 'A', 'C', 'T', 'G' };
     
@@ -15,14 +15,132 @@ static void GenerateMotifSequences(uint32_t nSeq, uint32_t seqLen, uint32_t moti
 
         offsets.push_back(rand() % offsetRange);
         memcpy(&curSeq[0] + offsets[i], &motif[0], motifLen);
-        seq.push_back(curSeq);
+        seqs.push_back(curSeq);
     }
+}
+
+struct SearchNode
+{
+    uint32_t offsetVal;
+    vector<bool> childOffsetSearched;
+    SearchNode(uint32_t curOFfset, uint32_t numChildren) : offsetVal(curOFfset) { childOffsetSearched.resize(numChildren, false); }
+};
+
+uint32_t GetConsensus(vector<string>& seqs, vector<uint32_t>& offsets, uint32_t motifLen)
+{
+    vector<uint32_t> aScore(motifLen, 0);
+    vector<uint32_t> tScore(motifLen, 0);
+    vector<uint32_t> cScore(motifLen, 0);
+    vector<uint32_t> gScore(motifLen, 0);
+
+    uint32_t consensus = 0;
+
+    for (uint32_t i = 0; i < seqs.size(); i++)
+    {
+        for (uint32_t j = 0; j < motifLen; j++)
+        {
+            char base = seqs[i][offsets[i] + j];
+
+            switch (base)
+            {
+            case 'A':
+
+                aScore[j]++;
+                break;
+
+            case 'T':
+
+                tScore[j]++;
+                break;
+
+            case 'C':
+
+                cScore[j]++;
+                break;
+
+            case 'G':
+
+                gScore[j]++;
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+
+    for (uint32_t i = 0; i < motifLen; i++)
+    {
+        uint32_t best = aScore[i];
+        if (tScore[i] > best) best = tScore[i];
+        if (cScore[i] > best) best = cScore[i];
+        if (gScore[i] > best) best = gScore[i];
+
+        consensus += best;
+    }
+
+    return consensus;
+}
+
+ResultCode FindMotif(vector<string>& seqs, uint32_t motifLen, vector<uint32_t>& offsets)
+{
+    vector<SearchNode> stack;
+
+    const uint32_t seqLen       = seqs[0].length();
+    const uint32_t offsetRange  = seqLen - motifLen;
+
+    offsets.resize(seqs.size(), 0);
+
+    uint32_t bestConsensus = 0;
+
+    for (uint32_t i = 0; i < seqs.size(); i++)
+    {
+        stack.push_back(SearchNode(i, offsetRange));
+
+        while (!stack.empty())
+        {
+            if (stack.size() == seqs.size())
+            {
+                vector<uint32_t> curOffsets(seqs.size(), 0);
+                for (uint32_t j = 0; j < stack.size(); j++) curOffsets[j] = stack[j].offsetVal;
+                uint32_t consensusScore = GetConsensus(seqs, curOffsets, motifLen);
+
+                if (consensusScore > bestConsensus)
+                {
+                    bestConsensus = consensusScore;
+                    memcpy(&offsets[0], &curOffsets[0], seqs.size() * sizeof(uint32_t));
+                }
+
+                stack.pop_back();
+                continue;
+            }
+
+            auto& cur = stack.back();
+
+            bool allChildrenSearched = true;
+
+            for (uint32_t j = 0; j < cur.childOffsetSearched.size(); j++)
+            {
+                if (!cur.childOffsetSearched[j])
+                {
+                    cur.childOffsetSearched[j] = true;
+                    stack.push_back(SearchNode(j, offsetRange));
+                    allChildrenSearched = false;
+                    break;
+                }
+            }
+
+            if (allChildrenSearched) stack.pop_back();
+        }
+    }
+
+    return OK;
 }
 
 void MotifFinding(vector<TestResult>& testResults)
 {
-    const uint32_t nSeq     = 20;
-    const uint32_t seqLen   = 100;
+    const uint32_t nSeq     = 6;
+    const uint32_t seqLen   = 10;
     const uint32_t motifLen = 6;
 
     vector<string> seq;
@@ -30,6 +148,10 @@ void MotifFinding(vector<TestResult>& testResults)
     vector<uint32_t> offsets;
 
     GenerateMotifSequences(nSeq, seqLen, motifLen, seq, motif, offsets);
+
+    vector<uint32_t> resultOffsets;
+
+    FindMotif(seq, motifLen, resultOffsets);
 
     __debugbreak();
 }
